bdd2bac1d68a2cf277f347f3c81de97225c61e0f
Awlad Hossen <mdawladhossenmito199922@gmail.com>
2022-11-24 21:52:37 +0600

    data lode api done

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 const app= express()
 
+app.use(cors())
+app.use(express.json())
 
 
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zlhkzur.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zlhkzur.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -18,10 +20,38 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 //middlewre
 
-app.use(cors())
-app.use(express.json())
 
- 
+
+async function run(){
+  try{
+   await client.connect()
+   const AgendaList = client.db("Hiring-Manager").collection("AgendaList");
+  
+   //AUTH
+  
+  
+  
+  
+  app.get('/AgendaList',async(req,res)=>{
+      const query ={}
+      const cursor = AgendaList.find(query)
+      const result = await cursor.toArray();
+      res.send(result)
+  })
+    
+  
+  
+  
+    
+  }
+  finally{
+  
+  }
+   
+  }
+
+  run().catch(console.dir)
+  
 app.listen(port, () => {
-  console.log("Auto Mobail running");
+  console.log("Auto Mobail running hhhh");
 });
